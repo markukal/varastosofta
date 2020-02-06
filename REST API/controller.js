ad884c8abdf86de0f57983c5dbf79ce979c7c96f
HuimaHeimo <46885479+HuimaHeimo@users.noreply.github.com>
2020-02-06 15:15:04 +0200

    REST API:n kehitys

---
+++ REST API/controller.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var sqlQuery;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: '',
+    password: '',
+    database: 'varasto',
+    timezone: 'Europe/Helsinki'
+
+});
+
+
+module.exports =
+{
+    
+}
