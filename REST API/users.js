44c6e1417d678b76e3bd2a9fe241c4918743ba40
iojani <8681846+iojani@users.noreply.github.com>
2020-02-14 14:32:57 +0200

    kayttajanimi muutettu -> kayttajatunnus

--- REST API/users.js
+++ REST API/users.js
@@ -11,13 +11,13 @@ module.exports =
             var luokkaID = req.query.luokkaID;
             var etunimi = req.query.etunimi;
             var sukunimi = req.query.sukunimi;
-            var kayttajanimi = req.query.kayttajanimi;
+            var kayttajatunnus = req.query.kayttajatunnus;
             // salasanan suolaus + hashaus
             const hashedPassword = await bcrypt.hash(req.query.salasana, 10);
             var kayttoOikeus = req.query.kayttoOikeus;
             // console.log(hashedPassword);
 
-            connection.query('INSERT INTO kayttajat (luokkaID, etunimi, sukunimi, kayttajanimi, salasana, kayttoOikeus) VALUES ("' + luokkaID + '", "'+ etunimi + '", "' + sukunimi + '", "' + kayttajanimi + '", "' + hashedPassword + '", "' + kayttoOikeus + '")', function(error, results, fields) {
+            connection.query('INSERT INTO kayttajat (luokkaID, etunimi, sukunimi, kayttajatunnus, salasana, kayttoOikeus) VALUES ("' + luokkaID + '", "'+ etunimi + '", "' + sukunimi + '", "' + kayttajatunnus + '", "' + hashedPassword + '", "' + kayttoOikeus + '")', function(error, results, fields) {
                 if (error) {
                     console.log("Virhe lisättäessä uutta käyttäjää, syy " + error);
                     res.send({"status":500, "error": error, "response": null});
