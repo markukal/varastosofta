37f9bc76b03bfef6266df93961fcd18bc02db4bd
iojani <8681846+iojani@users.noreply.github.com>
2020-03-24 16:43:15 +0200

    käyttäjien muokkaaminen

--- control/users.js
+++ control/users.js
@@ -10,7 +10,7 @@ module.exports =
         // haetaan kaikki käyttäjän kentät paitsi salasana
         fetchAll: function (req, res) {
             var columns = ['kayttajaID', 'luokat.nimi AS luokanNimi', 'kayttajatunnus', 'kayttoOikeus']
-            connection.query('SELECT kayttajaID, luokat.nimi AS luokanNimi, kayttajatunnus, kayttoOikeus FROM kayttajat INNER JOIN luokat ON kayttajat.luokkaID = luokat.luokkaID', function(error, results, fields) {
+            connection.query('SELECT kayttajaID, luokat.nimi AS luokanNimi, kayttajatunnus, salasana, kayttoOikeus FROM kayttajat INNER JOIN luokat ON kayttajat.luokkaID = luokat.luokkaID', function(error, results, fields) {
                 if (error) {
                     console.log("Virhe haettaessa käyttäjiä, syy " + error);
                     res.send({"status":500, "error": error, "response": null});
@@ -25,46 +25,83 @@ module.exports =
         register: async function (req, res) {
             var luokkaID = req.query.luokkaID;
             var kayttajatunnus = req.query.kayttajatunnus;
-            // salasanan suolaus + hashaus
-            const hashedPassword = await bcrypt.hash(req.query.salasana, 10);
-            var kayttoOikeus = req.query.kayttoOikeus;
-            // console.log(hashedPassword);
 
-            var columns = ['luokkaID', 'kayttajatunnus', 'salasana', 'kayttoOikeus']
-            connection.query('INSERT INTO ?? (??) VALUES (?,?,?,?)', ['kayttajat', columns, luokkaID, kayttajatunnus, hashedPassword, kayttoOikeus], function(error, results, fields) {
-                if (error) {
-                    console.log("Virhe lisättäessä uutta käyttäjää, syy " + error);
-                    res.send({"status":500, "error": error, "response": null});
-                }
-                else
-                {
-                    console.log("Rekisteröinti onnistui.");
-                    res.send({"status": 201});
-                }
-            });
+            if(req.query.salasana === req.query.salasanauudelleen) {
+
+                // salasanan suolaus + hashaus
+                const hashedPassword = await bcrypt.hash(req.query.salasana, 10);
+                var kayttoOikeus = req.query.kayttoOikeus;
+                // console.log(hashedPassword);
+
+                var columns = ['luokkaID', 'kayttajatunnus', 'salasana', 'kayttoOikeus']
+                connection.query('INSERT INTO ?? (??) VALUES (?,?,?,?)', ['kayttajat', columns, luokkaID, kayttajatunnus, hashedPassword, kayttoOikeus], function(error, results, fields) {
+                    if (error) {
+                        console.log("Virhe lisättäessä uutta käyttäjää, syy " + error);
+                        res.send({"status":500, "error": error, "response": null});
+                    }
+                    else
+                    {
+                        console.log("Rekisteröinti onnistui.");
+                        res.send({"status": 201});
+                    }
+                });
+            }
+            else // jos salasana ja salasana uudelleen eivät täsmää 
+            {
+                console.log("salasanat ei tasmaa");
+                res.send();
+            }
         },
         update: async function (req, res) {
-            var kayttajaID = req.query.kayttajaID;
-            var luokkaID = req.query.luokkaID;
-            var kayttajatunnus = req.query.kayttajatunnus;
-            var salasana = req.query.salasana;
-            var kayttoOikeus = req.query.kayttoOikeus;
-            
-            const hashedPassword = await bcrypt.hash(req.query.salasana, 10);
+            var kayttajaID = req.query.mkayttajaID;
+            var luokkaID = req.query.mluokkaID;
+            var kayttajatunnus = req.query.mkayttajatunnus;
+            var salasana = req.query.msalasana;
+            var kayttoOikeus = req.query.mkayttoOikeus;
 
-            connection.query('UPDATE kayttajat SET luokkaID = ?, kayttajatunnus = ?, salasana = ?, kayttoOikeus = ? WHERE kayttajatunnus = kayttajatunnus', [luokkaID, kayttajatunnus, hashedPassword, kayttoOikeus], function(error, results, fields) {
+
+            if(req.query.msalasana === req.query.msalasanauudelleen) {
+                const hashedPassword = await bcrypt.hash(req.query.msalasana, 10);
+
+                // tarkastetaan onko käyttäjän antama käyttäjätunnus sama kuin jo ennestään oleva
+                connection.query('SELECT * FROM kayttajat WHERE kayttajatunnus = ?', [kayttajatunnus], function(error, rows, fields) {
                     if (error) {
-                    console.log("Virhe muokattaessa käyttäjää, syy " + error);
-                    res.send({"status":500, "error": error, "response": null});
-                }
-                else 
-                {
-                    console.log("Käyttäjän muokkaus onnistui.");
-                    res.send({"status": 201});
-                }
-        });
+                        console.log("testtesttest");
+                        console.log("Virhe muokattaessa käyttäjää, syy " + error);
+                        res.send({"status":500, "error": error, "response": null});
+                    }
+                    // jos käyttäjätunnus on sama niin päivitetään kaikki muut kentät paitsi kayttajatunnus
+                    if (rows[0].kayttajatunnus === kayttajatunnus) {
+                        connection.query('UPDATE kayttajat SET luokkaID = ?, salasana = ?, kayttoOikeus = ? WHERE kayttajatunnus = kayttajatunnus', [luokkaID, hashedPassword, kayttoOikeus], function(error, results, fields) {
+                            console.log("Käyttäjän muokkaus onnistui.");
+                            res.send({"status": 201});
+                        });
+                    } 
+                    else 
+                    {
 
-    },
+                        // jos käyttäjän syöttämä käyttäjätunnus on eri kuin ennestään oleva, päivitetään se
+                        connection.query('UPDATE kayttajat SET luokkaID = ?, kayttajatunnus = ?, salasana = ?, kayttoOikeus = ? WHERE kayttajatunnus = kayttajatunnus', [luokkaID, kayttajatunnus, hashedPassword, kayttoOikeus], function(error, results, fields) {
+                            if (error) {
+                                console.log("Virhe muokattaessa käyttäjää, syy " + error);
+                                res.send({"status":500, "error": error, "response": null});
+                            }
+                            else 
+                            {
+                                console.log("Käyttäjän muokkaus onnistui.");
+                                res.send({"status": 201});
+                            }
+                        });
+                    }
+                });
+            }
+            else // jos salasana ja salasana uudelleen eivät täsmää
+            {
+                // TODO: connect-flash errorviestit,, mahdollisesti myös muihin kenttiin 
+                console.log("salasanat ei tasmaa");
+                res.send();
+            }
+        },
         delete: function (req, res) {
             // TODO: tarkistus ettei kirjautuneena oleva käyttäjä pysty poistamaan itseään
             // TAI jonkinlainen pääkäyttäjä jota ei pysty poistamaan
@@ -80,8 +117,8 @@ module.exports =
                     console.log("Käyttäjän poistaminen onnistui.");
                     res.send({"status": 201});
                 }
-                });
-            }
+            });
+        }
 
     }
 
