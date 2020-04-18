b5434ccd58f109dbe8540d64bdeda14191b87550
iojani <8681846+iojani@users.noreply.github.com>
2020-04-18 21:23:12 +0300

    toiminnallisuus tarvikkeiden otolle

--- control/varastotapahtumat.js
+++ control/varastotapahtumat.js
@@ -2,93 +2,99 @@ var sqlQuery;
 var connection = require('./connection');
 
 module.exports = 
-{
-    //Tietojen haku varastotapahtumat taulusta
-    fetchAll: function (req, res){
-        //Haetaan kaikki tiedot jos hakuehtoja ei tule
-        sqlQuery = "SELECT varastotapahtumat.tapahtumaID AS ID, varastotapahtumat.kasittelija AS kasittelija, varastotapahtumat.ttyyppinimi AS tapahtumatyyppinimi, " +
-        "varastotapahtumat.tarvikenimi AS tarvikenimi, varastotapahtumat.luokkanimi AS luokka, varastotapahtumat.maara AS maara, varastotapahtumat.yksikkonimi AS yksikko, " +
-        "varastotapahtumat.tarvikehpaikka AS tarvikepaikka, varastotapahtumat.pvm AS pvm FROM varastotapahtumat";
+	{
+		//Tietojen haku varastotapahtumat taulusta
+		fetchAll: function (req, res){
+			//Haetaan kaikki tiedot jos hakuehtoja ei tule
+			sqlQuery = "SELECT varastotapahtumat.tapahtumaID AS ID, varastotapahtumat.kasittelija AS kasittelija, varastotapahtumat.ttyyppinimi AS tapahtumatyyppinimi, " +
+				"varastotapahtumat.tarvikenimi AS tarvikenimi, varastotapahtumat.luokkanimi AS luokka, varastotapahtumat.maara AS maara, varastotapahtumat.yksikkonimi AS yksikko, " +
+				"varastotapahtumat.tarvikehpaikka AS tarvikepaikka, varastotapahtumat.pvm AS pvm FROM varastotapahtumat";
 
-        var i = 0;//Apumuuttuja
-        
-        //Käydään req.query läpi ja lisätään hakuun ehdot 
-        for (var key in req.query){
+			var i = 0;//Apumuuttuja
 
-            if(!req.query[key] == "" || !req.query[key] == undefined || !req.query[key] == null){
-                if(i > 0){
-                    sqlQuery = sqlQuery + " OR "+ key + " LIKE '%" + req.query[key] + "%'";
-                }
-                else {
-                    sqlQuery = sqlQuery + " WHERE " + key + " LIKE '%" + req.query[key] + "%'";
-                    i++;
-                }
-            }
-        }
-        connection.query(sqlQuery, function (error, results, fields) {
-            if (error) {
-                console.log("Virhe haettaessa dataa varastotapahtumat-taulusta, syy: " + error);
-                res.send({ "status": 500, "error": error, "response": null });
-            }
-            else {
-                console.log(results);
-                res.json(results);
-            }
-        });
-    },
+			//Käydään req.query läpi ja lisätään hakuun ehdot 
+			for (var key in req.query){
 
-    //Uuden varastotapahtuman lisäys
-    addNew: function (req, res){
-        sqlQuery = "INSERT INTO varastotapahtumat (tapahtumaID, ttyyppinimi, luokkanimi, tarvikenimi, tarvikehpaikka, maara, yksikkonimi, kasittelija, pvm) VALUES (" + null + ", " + 
-        connection.escape(req.query.ttyyppinimi) + ", " + connection.escape(req.query.luokkanimi) +", " + connection.escape(req.query.tarvikenimi) + ", " +
-        connection.escape(req.query.tarvikehpaikka) + ", " + connection.escape(req.query.maara) + ", " + connection.escape(req.query.yksikkonimi) + ", " +
-        connection.escape(req.query.kasittelija) + ", " + connection.escape(req.query.pvm) + ");";
+				if(!req.query[key] == "" || !req.query[key] == undefined || !req.query[key] == null){
+					if(i > 0){
+						sqlQuery = sqlQuery + " OR "+ key + " LIKE '%" + req.query[key] + "%'";
+					}
+					else {
+						sqlQuery = sqlQuery + " WHERE " + key + " LIKE '%" + req.query[key] + "%'";
+						i++;
+					}
+				}
+			}
+			connection.query(sqlQuery, function (error, results, fields) {
+				if (error) {
+					console.log("Virhe haettaessa dataa varastotapahtumat-taulusta, syy: " + error);
+					res.send({ "status": 500, "error": error, "response": null });
+				}
+				else {
+					console.log(results);
+					res.json(results);
+				}
+			});
+		},
 
-        console.log(sqlQuery);
-        connection.query(sqlQuery, function (error, results, fields) {
-            if (error) {
-                console.log("Virhe lisätessä dataa varastotapahtumat-tauluun, syy: " + error);
-                res.send({ "status": 500, "error": error, "response": null });
-            }
-            else {
-                res.json(results);
-            }
-        });
-    },
+		//Uuden varastotapahtuman lisäys
+		addNew: function (req, res){
+			var pvm = new Date().toJSON().slice(0, 19).replace('T', ' ');
+			var luokkaID = req.query.luokkaID;
+			var nimi = req.query.nimi;
+			var maara = req.query.maara;
+			var yksikkoID = req.query.yksikkoID;
+			var kasittelija = req.query.kasittelija;
+			var ttyyppinimi = req.query.ttyyppinimi;
+			console.log(luokkaID);
+			console.log(ttyyppinimi);
 
-    //Varastotapahtuman tietojen päivitys
-    update: function (req, res){
-        sqlQuery = "UPDATE varastotapahtumat SET ttyyppinimi ="+ connection.escape(req.query.ttyyppinimi) + ", tarvikenimi =" + connection.escape(req.query.tarvikenimi) + 
-        ", luokkanimi ='" + connection.escape(req.query.luokkanimi) + "', tarvikenimi ='" + connection.escape(req.query.tarvikenimi) + 
-        "', tarvikehpaikka ='" + connection.escape(req.query.tarvikehpaikka) + "', maara="+ connection.escape(req.query.maara) + 
-        "', yksikkonimi ='" + connection.escape(req.query.yksikkonimi) + "', kasittelija ='" + connection.escape(req.query.kasittelija) +
-        "', pvm=" + connection.escape(req.query.pvm) + " WHERE tapahtumaID=" + connection.escape(req.query.tapahtumaID);
+			connection.query('INSERT INTO varastotapahtumat (ttyyppinimi, luokkanimi, tarvikenimi, maara, yksikkonimi, kasittelija, pvm) VALUES (?, ?, ?, ?, ?, ?, ?)', [ttyyppinimi, luokkaID, nimi, maara, yksikkoID, kasittelija, pvm], function (error, results) {
+				if (error) {
+					console.log("Virhe luodessa varastotapahtumaa (OTTO), syy " + error);
+					res.status(400);
+				}
+				else 
+				{
+					console.log("Varastotapahtuman luonti onnistui (OTTO)");
+					res.send({"status": 201});
+				}
+			});
+		},
 
-        connection.query(sqlQuery, function (error, results, fields) {
-            if (error) {
-                console.log("Virhe päivittäessä dataa varastotapahtumat-tauluun, syy: " + error);
-                res.send({ "status": 500, "error": error, "response": null });
-            }
-            else {
-                res.json(results);
-            }
-        });
-    },
+		//Varastotapahtuman tietojen päivitys
+		update: function (req, res){
+			sqlQuery = "UPDATE varastotapahtumat SET ttyyppinimi ="+ connection.escape(req.query.ttyyppinimi) + ", tarvikenimi =" + connection.escape(req.query.tarvikenimi) + 
+				", luokkanimi ='" + connection.escape(req.query.luokkanimi) + "', tarvikenimi ='" + connection.escape(req.query.tarvikenimi) + 
+				"', tarvikehpaikka ='" + connection.escape(req.query.tarvikehpaikka) + "', maara="+ connection.escape(req.query.maara) + 
+				"', yksikkonimi ='" + connection.escape(req.query.yksikkonimi) + "', kasittelija ='" + connection.escape(req.query.kasittelija) +
+				"', pvm=" + connection.escape(req.query.pvm) + " WHERE tapahtumaID=" + connection.escape(req.query.tapahtumaID);
 
-    //Varastotapahtuman poisto
-    delete: function (req, res){
-        sqlQuery = "DELETE FROM varastotapahtumat WHERE tapahtumaID=" + connection.escape(req.query.tapahtumaID);
+			connection.query(sqlQuery, function (error, results, fields) {
+				if (error) {
+					console.log("Virhe päivittäessä dataa varastotapahtumat-tauluun, syy: " + error);
+					res.send({ "status": 500, "error": error, "response": null });
+				}
+				else {
+					res.json(results);
+				}
+			});
+		},
 
-        connection.query(sqlQuery, function (error, results, fields) {
-            if (error) {
-                console.log("Virhe poistaessa dataa varastotapahtumat-taulusta, syy: " + error);
-                res.send({ "status": 500, "error": error, "response": null });
-            }
-            else {
+		//Varastotapahtuman poisto
+		delete: function (req, res){
+			sqlQuery = "DELETE FROM varastotapahtumat WHERE tapahtumaID=" + connection.escape(req.query.tapahtumaID);
 
-                res.json(results);
-            }
-        });  
-    }
+			connection.query(sqlQuery, function (error, results, fields) {
+				if (error) {
+					console.log("Virhe poistaessa dataa varastotapahtumat-taulusta, syy: " + error);
+					res.send({ "status": 500, "error": error, "response": null });
+				}
+				else {
 
-}
+					res.json(results);
+				}
+			});  
+		}
+
+	}
