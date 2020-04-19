ce60227d8538f4c8bf402a3d35e922c23c5e206d
iojani <8681846+iojani@users.noreply.github.com>
2020-04-19 20:37:48 +0300

    hpaikka haku

--- control/klistat.js
+++ control/klistat.js
@@ -18,28 +18,12 @@ module.exports =
     // Hakee klistat-taulusta kaikki rivit. Jos tulee parametrejä, suodatetaan tulos niiden mukaan.
     fetchAll: function (req, res) {
             
-        if (req.query.klistaID == "" &&  req.query.tarvikeID == "") {
-            sqlQuery = "SELECT klistat.klistaID as ID, tarvikkeet.nimi AS nimi, yksikot.nimi AS yksikko, tarvikkeet.kuvaus AS kuvaus, tarvikkeet.maara AS maara, tarvikkeet.hinta AS hinta, varastot.nimi AS varasto FROM klistat " + 
+       if (req.query.klistaID == undefined && req.query.tarvikeID == undefined) {
+            sqlQuery = "SELECT klistat.klistaID as ID, tarvikkeet.nimi AS nimi, yksikot.nimi AS yksikko, tarvikkeet.kuvaus AS kuvaus, tarvikkeet.maara AS maara, tarvikkeet.hinta AS hinta, tarvikkeet.hpaikka AS hpaikka, varastot.nimi AS varasto FROM klistat " + 
             "INNER JOIN tarvikkeet ON klistat.tarvikeID = tarvikkeet.tarvikeID " +
             "INNER JOIN varastot ON tarvikkeet.varastoID = varastot.varastoID " +
             "INNER JOIN yksikot ON tarvikkeet.yksikkoID = yksikot.yksikkoID;";
-
-            // Vaihtoehtoinen hakulauseke toisenlaiseen klistat-taulun rakenteeseen.
-            // sqlQuery = "SELECT klistat.klistaID, tarvikkeet.nimi AS Nimi, klistat.maara AS Määrä, klistat.hinta AS Hinta, klistat.pvm AS Päivämäärä FROM klistat " + 
-           // "INNER JOIN tarvikkeet ON klistat.tarvikeID = tarvikkeet.tarvikeID;"
-
-        }
-        else if (req.query.klistaID == undefined && req.query.tarvikeID == undefined) {
-            sqlQuery = "SELECT klistat.klistaID as ID, tarvikkeet.nimi AS nimi, yksikot.nimi AS yksikko, tarvikkeet.kuvaus AS kuvaus, tarvikkeet.maara AS maara, tarvikkeet.hinta AS hinta, varastot.nimi AS varasto FROM klistat " + 
-            "INNER JOIN tarvikkeet ON klistat.tarvikeID = tarvikkeet.tarvikeID " +
-            "INNER JOIN varastot ON tarvikkeet.varastoID = varastot.varastoID " +
-            "INNER JOIN yksikot ON tarvikkeet.yksikkoID = yksikot.yksikkoID;";
-
-            // Vaihtoehtoinen hakulauseke toisenlaiseen klistat-taulun rakenteeseen.
-           // sqlQuery = "SELECT klistat.klistaID, tarvikkeet.nimi AS Nimi, klistat.maara AS Määrä, klistat.hinta AS Hinta, klistat.pvm AS Päivämäärä FROM klistat " + 
-           // "INNER JOIN tarvikkeet ON klistat.tarvikeID = tarvikkeet.tarvikeID;"
-        }
-
+       }
 
         //Lokitetaan hakulauseke palvelimelle debuggausta varten. Tietoturvasyistä oletuksena kommentoitu
         console.log(sqlQuery);
@@ -51,8 +35,6 @@ module.exports =
                 res.send({ "status": 500, "error": error, "response": null });
             }
             else {
-                /*console.log("Data = " + JSON.stringify(results));
-                console.log("Params = " + JSON.stringify(req.query));*/
 
                 res.json(results);
             }
@@ -79,8 +61,6 @@ module.exports =
                 res.send({ "status": 500, "error": error, "response": null });
             }
             else {
-                /*console.log("Data = " + JSON.stringify(results));
-                console.log("Params = " + JSON.stringify(req.query));*/
                 res.json(results);
             }
         });
@@ -106,8 +86,6 @@ module.exports =
                 res.send({ "status": 500, "error": error, "response": null });
             }
             else {
-                /*console.log("Data = " + JSON.stringify(results));
-                console.log("Params = " + JSON.stringify(req.query));*/
 
                 res.json(results);
             }
@@ -134,8 +112,6 @@ module.exports =
                     res.send({ "status": 500, "error": error, "response": null });
                 }
                 else {
-                    /*console.log("Data = " + JSON.stringify(results));
-                    console.log("Params = " + JSON.stringify(req.query));*/
     
                     res.json(results);
                 }
